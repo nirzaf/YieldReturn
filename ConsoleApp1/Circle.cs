c408fc0660dcd145495daabc11cfc1b8c0057165
nirzaf <mfmfazrin1986@gmail.com>
2022-12-06 22:44:33 +0530

    Circle

---
+++ ConsoleApp1/Circle.cs
@@ -0,0 +1,19 @@
+﻿public struct Circle
+{
+    public double Radius { get; set; }
+    private double Area => Math.PI * Radius * Radius;
+    
+    private double Circumference => 2 * Math.PI * Radius;
+    
+    //Get Area of Circle
+    public double GetArea()
+    {
+        return Area;
+    }
+    
+    //Get Circumference of Circle
+    public double GetCircumference()
+    {
+        return Circumference;
+    }
+}
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,34 +10,77 @@ internal class Program
     {
         WriteLine("Printing from C#");
 
+        /*Circle c1 = new()
+        {
+            Radius = 7
+        };
+        
+        WriteLine(c1.GetArea());
+        WriteLine(c1.GetCircumference());
+
+        ReadLine();*/
+
         // Generate 100 Employees using Bogus
         var listEmployee = new Faker<Employee>()
             .RuleFor(o => o.Id, f => f.IndexFaker)
             .RuleFor(o => o.Name, f => f.Name.FullName())
             .RuleFor(o => o.Age, f => f.Random.Int(18, 60))
-            .GenerateBetween(500000, 500000);
+            .GenerateBetween(500000, 500000).ToArray();
 
         // Log the duration to filter all employees with age > 30
         var watch = StartNew();
-        var filteredList = listEmployee.Where(x => x.Age > 30).ToList();
+        var filteredList = listEmployee.Where(x => x.Age > 30);
         watch.Stop();
 
-        WriteLine($"Filtering took {filteredList.Count} using LINQ {watch.ElapsedTicks} ticks");
+        WriteLine($"Filtering took {filteredList.Count()} using LINQ {watch.ElapsedTicks} ticks");
+
+        /*
+        foreach (var emp in filteredList.Take(10))
+        {
+            WriteLine($"{emp.Id} - {emp.Name} - {emp.Age}");
+        }
+        */
 
         // Log the duration to filter all employees with age > 30
         watch = StartNew();
-        var filtered = FilterAllEmployeesAbove30(listEmployee);
+        var filtered = FilterAllEmployeesAbove30(filteredList);
         watch.Stop();
 
         WriteLine($"Filtering took {filtered.Count()} using yield {watch.ElapsedTicks} ticks");
 
         ReadLine();
-        IEnumerable<Employee> FilterAllEmployeesAbove30(List<Employee> listEmployee)
+        IEnumerable<Employee> FilterAllEmployeesAbove30(IEnumerable<Employee> listEmployee)
         {
             foreach (Employee emp in listEmployee.Where(emp => emp.Age > 30))
             {
                 yield return emp;
             }
         }
+    }
+}
+
+//Implement a Queue using two Stacks
+
+public class Queue<T> where T : class
+{
+    private readonly Stack<T> _stack1 = new();
+    private readonly Stack<T> _stack2 = new();
+
+    public void Enqueue(T item)
+    {
+        _stack1.Push(item);
+    }
+
+    public T Dequeue()
+    {
+        if (_stack2.Count == 0)
+        {
+            while (_stack1.Count > 0)
+            {
+                _stack2.Push(_stack1.Pop());
+            }
+        }
+
+        return _stack2.Pop();
     }
 }
