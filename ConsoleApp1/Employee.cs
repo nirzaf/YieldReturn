ef1223749cb57abccc4f4a3e286fa7cf29f93ab1
nirzaf <mfmfazrin1986@gmail.com>
2022-12-06 21:34:15 +0530

    Generate 100 Employees using Bogus

---
+++ ConsoleApp1/Employee.cs
@@ -0,0 +1,6 @@
+﻿public class Employee
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int Age { get; set; }
+}
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,43 +4,40 @@ using Bogus;
 using static System.Console;
 using static System.Diagnostics.Stopwatch;
 
-WriteLine("Printing from C#");
-
-// Generate 100 Employees using Bogus
-var listEmployee = new Faker<Employee>()
-    .RuleFor(o => o.Id, f => f.IndexFaker)
-    .RuleFor(o => o.Name, f => f.Name.FullName())
-    .RuleFor(o => o.Age, f => f.Random.Int(18, 60))
-    .GenerateBetween(500000, 500000);
-
-// Log the duration to filter all employees with age > 30
-var watch = StartNew();
-var filteredList = listEmployee.Where(x => x.Age > 30).ToList();
-watch.Stop();
-
-WriteLine($"Filtering took {filteredList.Count} using LINQ {watch.ElapsedTicks} ticks");
-
-// Log the duration to filter all employees with age > 30
-watch = StartNew();
-var filtered = FilterAllEmployeesAbove30(listEmployee);
-watch.Stop();
-
-WriteLine($"Filtering took {filtered.Count()} using yield {watch.ElapsedTicks} ticks");
-
-ReadLine();
-IEnumerable<Employee> FilterAllEmployeesAbove30(List<Employee> listEmployee)
+internal class Program
 {
-    foreach (Employee emp in listEmployee.Where(emp => emp.Age > 30))
+    public static void Main(string[] args)
     {
-        yield return emp;
+        WriteLine("Printing from C#");
+
+        // Generate 100 Employees using Bogus
+        var listEmployee = new Faker<Employee>()
+            .RuleFor(o => o.Id, f => f.IndexFaker)
+            .RuleFor(o => o.Name, f => f.Name.FullName())
+            .RuleFor(o => o.Age, f => f.Random.Int(18, 60))
+            .GenerateBetween(500000, 500000);
+
+        // Log the duration to filter all employees with age > 30
+        var watch = StartNew();
+        var filteredList = listEmployee.Where(x => x.Age > 30).ToList();
+        watch.Stop();
+
+        WriteLine($"Filtering took {filteredList.Count} using LINQ {watch.ElapsedTicks} ticks");
+
+        // Log the duration to filter all employees with age > 30
+        watch = StartNew();
+        var filtered = FilterAllEmployeesAbove30(listEmployee);
+        watch.Stop();
+
+        WriteLine($"Filtering took {filtered.Count()} using yield {watch.ElapsedTicks} ticks");
+
+        ReadLine();
+        IEnumerable<Employee> FilterAllEmployeesAbove30(List<Employee> listEmployee)
+        {
+            foreach (Employee emp in listEmployee.Where(emp => emp.Age > 30))
+            {
+                yield return emp;
+            }
+        }
     }
 }
-
-
-public class Employee
-{
-    public int Id { get; set; }
-    public string Name { get; set; }
-    public int Age { get; set; }
-}
-
